4e3e12227abd6f4528865a00a62fa2cf762ef45b
bernardo <salsa@192.168.1.27>
2025-11-03 19:57:54 -0300

    primeiro commit

---
+++ quiz.java
@@ -0,0 +1,51 @@
+
+
+public class Pergunta {
+    public boolean verificarResposta(String r){
+        String resposta = getResposta();
+        if(r==resposta)
+    }
+
+    private getResposta() {
+    
+    }
+
+    private setPergunta() {
+    
+    }
+
+
+public class PerguntaMultiplaEscolha extends Pergunta {
+    
+}
+
+public class PerguntaVerdadeiroFalse extends Pergunta {
+
+}
+
+class Jogador {
+
+}
+
+public class Quiz {
+    
+    static numeroDeQuizes;
+
+    static getTotalQuizzesJogos() {
+    
+    }
+
+    public static void main(String[] args) {
+    
+    }
+}
+
+class Pontuavel {
+    public int calcularPontuacao() {
+    
+    }
+}
+
+class SistemaLogs {
+    
+}
