791076d35ca45beca5d49c9fe4d62ca125bbd180
bernardo <bernardinhogamespray@gmail.com>
2025-11-06 17:09:13 -0300

    adições

--- quiz.java
+++ quiz.java
@@ -1,7 +1,14 @@
+//criar uma array de strings com as respostas e perguntas onde um item tem o mesmo índice que o seu correspondente
+
 public class Pergunta {
     public boolean validarResposta(String r){
-        String respostaCerta
-        if(r==resposta)
+      String respostaCerta
+      if(r==resposta) {
+        return true
+      }
+      else {
+      return false
+      } 
     }
 
     private getResposta() {
@@ -14,7 +21,7 @@ public class Pergunta {
 
 
 public class PerguntaMultiplaEscolha extends Pergunta {
-    
+
 }
 
 public class PerguntaVerdadeiroFalse extends Pergunta {
@@ -31,7 +38,7 @@ public class Quiz {
     static int numeroDeQuizes = 0;
 
     static int getTotalQuizzesJogos() {
-        //printar numeroDeQuizes;
+        System.out.println("Você jogou o quiz" + numeroDeQuizes + "vezes!");
     }
 
     public static void main(String[] args) {
@@ -39,26 +46,38 @@ public class Quiz {
         int p = 0;
         while (p < 10) {
             Pontuavel pontosAcumulados;
-            pontosAcumulados = new Pontuavel();   
+            pontosAcumulados = new Pontuavel(); 
+
             //registrarEvento(Pontuavel pontosAcumulados);
+        
             Jogador novoJogador;
             novoJogador = new Jogador();
+
             //registrarEvento(Jogador novoJogador);
+         
             Pergunta novaPergunta;
             novaPergunta = new Pergunta();
+            
             //registrarEvento(Pergunta novaPergunta);
+            
             String perguntaAtual;
             perguntaAtual = novaPergunta.setPergunta();
+            
             //registrarEvento(String pergunta);
+            
             String respostaCerta;
             respostaCerta = novaPergunta.getResposta();
+
             //registrarEvento(String respostaCerta);
             //String resposta;
             //resposta = novoJogador.responder();
             //registrarEvento(String resposta, Jogador novoJogador);
+        
             boolean validacao;
             validacao = novaPergunta.validarResposta(resposta);
+
             //registrarEvento(String resposta);
+
             if (!validacao) {
                 System.out.println("A resposta está incorreta!");
             }
@@ -83,6 +102,6 @@ class Pontuavel {
 
 class SistemaLogs {
     void registrarEvento() {
-    
+      
     };
 }
